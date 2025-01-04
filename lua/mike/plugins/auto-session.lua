1dc1eb6d6c67e54b0bc837e4497d4bbde6c8fac8
Mike Pullen <mike.pullen@gmail.com>
2025-01-03 20:18:11 -0600

    disable auto-session

--- lua/mike/plugins/auto-session.lua
+++ lua/mike/plugins/auto-session.lua
@@ -1,8 +1,11 @@
-return {
-  'rmagatti/auto-session',
-  lazy = false,
-
-  opts = {
-    -- log_level = 'debug',
-  },
-}
+return {}
+--  TODO:  having trouble with auto save session and neo-tree
+--
+-- return {
+--   'rmagatti/auto-session',
+--   lazy = false,
+--
+--   opts = {
+--     -- log_level = 'debug',
+--   },
+-- }
